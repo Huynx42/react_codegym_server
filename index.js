e702504cb7025f8333b1166d239dd88f80c1edc4
Nguyen Xuan Huy <huynx42@due.edu.vn>
2022-06-22 20:00:17 +0700

    first server commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import posts from './routers/posts.js';
+import mongoose from 'mongoose';
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const URI = 'mongodb+srv://huynx42:1234@cluster0.cotvzhr.mongodb.net/?retryWrites=true&w=majority';
+
+app.use(bodyParser.json({ limit:'30mb' }));
+app.use(bodyParser.urlencoded({extended:true, limit:'30mb'}));
+app.use(cors());
+
+app.use('/posts', posts)
+
+mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Connected to DB');
+        app.listen(PORT, () => {
+        console.log(`Server is running at port ${PORT}`)
+        })        
+    }).catch((err) => {
+        console.error('err', err)
+    })
